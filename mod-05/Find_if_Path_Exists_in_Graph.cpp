72be6fe6a17a2128649f6e4eff8f84c6e4579cf5
nabil0203 <nabilahmed0203@gmail.com>
2025-02-09 01:33:40 +0600

    mod-5 update

---
+++ mod-05/Find_if_Path_Exists_in_Graph.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/find-if-path-exists-in-graph/description/
+
+
+
+class Solution
+{
+public:
+    vector<int> adj_list[200005];
+    bool vis[200005];
+
+    void DFS(int src)
+    {
+        vis[src] = true;
+
+        for (int child : adj_list[src])
+        {
+            if (!vis[child])
+            {
+                DFS(child);
+            }
+        }
+    }
+
+    bool validPath(int n, vector<vector<int>> &edges, int source, int destination)
+    {
+
+        for (int i = 0; i < edges.size(); i++)                      // converting the edge list into adj_list
+        {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            adj_list[a].push_back(b);
+            adj_list[b].push_back(a);
+        }
+
+        memset(vis, false, sizeof(vis));
+        DFS(source);
+
+        if (vis[destination] == true)
+            return true;
+        else
+            return false;
+    }
+};
---
+++ mod-05/Island_Perimeter.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/island-perimeter/description/
+
+
+
+class Solution
+{
+public:
+    bool vis[105][105];
+    vector<pair<int, int>> d = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    int n, m;
+    int cnt;
+
+    bool valid(int i, int j)
+    {
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return false;
+        return true;
+    }
+
+    void DFS(int si, int sj, vector<vector<int>> &grid)
+    {
+
+        vis[si][sj] = true;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int ci = si + d[i].first;
+            int cj = sj + d[i].second;
+
+            if (valid(ci, cj) == false)
+                cnt++;
+
+            else if (valid(ci, cj) == true && grid[ci][cj] == 0)
+                cnt++;
+
+            else if (valid(ci, cj) == true && !vis[ci][cj] && grid[ci][cj] == 1)
+            {
+                DFS(ci, cj, grid);
+            }
+        }
+    }
+
+    int islandPerimeter(vector<vector<int>> &grid)
+    {
+        cnt = 0;
+        n = grid.size();    // number of row
+        m = grid[0].size(); // number of col
+
+        memset(vis, false, sizeof(vis));
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                if (!vis[i][j] && grid[i][j] == 1)
+                    DFS(i, j, grid);
+        return cnt;
+    }
+};
